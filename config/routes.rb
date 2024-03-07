a73d0bbb0475507bed1f1ae8d8a779498195a767
suevin choi <suevin.choi@kellogg.northwestern.edu>
2024-03-07 19:08:19 +0000

    2

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,12 @@ Rails.application.routes.draw do
   resources "places"
   resources "sessions"
   resources "users"
+
+   # Login/logout
+   get("/login", { :controller => "sessions", :action => "new" })
+   get("/logout", { :controller => "sessions", :action => "destroy" })
+
+     # Define the root route
+    get("/", { :controller => "placess", :action => "index" })
+
 end
