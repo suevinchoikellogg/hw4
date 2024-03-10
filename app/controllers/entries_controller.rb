9c8e90a9eae77635de1f9306adfbff2a7142dae7
suevin choi <suevin.choi@kellogg.northwestern.edu>
2024-03-10 04:17:09 +0000

    commit

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -8,11 +8,19 @@ class EntriesController < ApplicationController
     @entry["title"] = params["title"]
     @entry["description"] = params["description"]
     @entry["occurred_on"] = params["occurred_on"]
-    @entry["image"] = entry["image"]
+    @entry["image_url"] = params["image_url"]
     @entry.uploaded_image.attach(params["uploaded_image"])
     @entry["place_id"] = params["place_id"]
     @entry.save
     redirect_to "/places/#{@entry["place_id"]}"
+  end
+
+  before_action :allow_cors
+  def allow_cors
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, PATCH, DELETE, OPTIONS'
+    response.headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token, Auth-Token, Email, X-User-Token, X-User-Email'
+    response.headers['Access-Control-Max-Age'] = '1728000'
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2022_03_09_020701) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_10_035357) do
   create_table "entries", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -19,6 +19,7 @@ ActiveRecord::Schema[7.1].define(version: 2022_03_09_020701) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_url"
   end
 
   create_table "places", force: :cascade do |t|
