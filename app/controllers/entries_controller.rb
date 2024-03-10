edc783cf7b0fa84e1c5807a8ff4155b8c90ecfb5
suevin choi <suevin.choi@kellogg.northwestern.edu>
2024-03-10 03:53:45 +0000

    Add image

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -8,6 +8,8 @@ class EntriesController < ApplicationController
     @entry["title"] = params["title"]
     @entry["description"] = params["description"]
     @entry["occurred_on"] = params["occurred_on"]
+    @entry["image"] = entry["image"]
+    @entry.uploaded_image.attach(params["uploaded_image"])
     @entry["place_id"] = params["place_id"]
     @entry.save
     redirect_to "/places/#{@entry["place_id"]}"
