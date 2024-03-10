399e915620b8386e391f4109313c65b938d2e1e5
suevin choi <suevin.choi@kellogg.northwestern.edu>
2024-03-10 05:13:49 +0000

    Session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -21,7 +21,7 @@ class SessionsController < ApplicationController
 
   def destroy
     session["user_id"] = nil
-    flash["notice"] = "Goodbye."
+    flash["notice"] = "Good bye!"
     redirect_to "/login"
   end
 end
